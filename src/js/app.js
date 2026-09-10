91190cb3104261121d03b2dea45d8e260eb168c0
AdriAM44 <adrianssr58@gmail.com>
2026-09-10 23:46:58 +0000

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,46 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "position-right", // social media bar position (position-left or position-right)
-        //for social media links, only update usernames
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+
+  // 1. Portada (Cover)
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+  // 2. Nombre y Apellido
+  let name = variables.name ? variables.name : "Mr";
+  let lastName = variables.lastName ? variables.lastName : "Robot";
+
+  // 3. Rol
+  let role = variables.role ? variables.role : "Hacker";
+
+  // 4. Ciudad y País
+  let city = variables.city ? variables.city : "New York";
+  let country = variables.country ? variables.country : "USA";
+
+  // 5. Posición de redes sociales (position-left o position-right)
+  let socialMediaPosition = variables.socialMediaPosition
+    ? variables.socialMediaPosition
+    : "position-right";
+
+  // 6. Redes sociales
+  let twitter = variables.twitter ? variables.twitter : "4geeksacademy";
+  let github = variables.github ? variables.github : "4geeksacademy";
+  let linkedin = variables.linkedin ? variables.linkedin : "4geeksacademy";
+  let instagram = variables.instagram ? variables.instagram : "4geeksacademy";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${socialMediaPosition}">
+            <li><a href="https://twitter.com/${twitter}" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${github}" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${linkedin}" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${instagram}" target="_blank"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +54,11 @@ window.onload = function() {
     // if includeCover is true the algorithm should show the cover image
     includeCover: true,
     // this is the image's url that will be used as a background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://raw.githubusercontent.com/syntax-samurai/fsociety/master/preview.png",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://static.wikia.nocookie.net/mrrobot/images/8/81/Season_4_Mr._Robot.jpg/revision/latest?cb=20240127174830",
     // social media bar position (position-left or position-right)
     socialMediaPosition: "position-right",
     // social media usernames
